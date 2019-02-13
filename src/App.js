0c426c8c05b66df2ae6f912b62f86244ddb7150b
randaman23 <randaman234@gmail.com>
2019-02-12 18:33:05 -0700

    beginning of some react hooks?

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
+function Example() {
+  const [count, setCount] = useState(0)
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+      <p>You clicked {count} times.</p>
+      <button onClick={() => setCount(count + 1)}>Clicketh Meh</button>
       </div>
     );
   }
-}
 
-export default App;
+
+export default Example
